79f53146a1c8e51175956a3d712a8d1978226859
Andy Pan <andy0130tw@yahoo.com.tw>
2024-07-14 23:58:41 +0800

    Gate setting nonblocking mode behind unix cfg

--- lib/wasix/src/syscalls/wasi/fd_fdstat_set_flags.rs
+++ lib/wasix/src/syscalls/wasi/fd_fdstat_set_flags.rs
@@ -32,35 +32,44 @@ pub fn fd_fdstat_set_flags(
     let fd_entry = wasi_try_ok!(fd_map.get_mut(&fd).ok_or(Errno::Badf));
     fd_entry.flags = flags;
 
-    // apply O_NONBLOCK state to the underlying FD
+    #[cfg(unix)]
+    {
+        // apply O_NONBLOCK state to the underlying file descriptor via fcntl
+        let guard = fd_entry.inode.read();
 
-    let guard = fd_entry.inode.read();
-
-    let maybe_sys_fd = match guard.deref() {
-        Kind::File { handle, .. } => {
-            if let Some(handle) = handle {
-                let handle = handle.clone();
-                let handle = wasi_try_ok!(handle.read().map_err(|_| { Errno::Badf }));
-                handle.get_special_fd()
-            } else { None }
-        },
-        _ => None,
-    };
-
-    if let Some(sys_fd) = maybe_sys_fd {
-        let sys_fd = wasi_try_ok!(i32::try_from(sys_fd).map_err(|_| { Errno::Badf }));
-        let fcntl_flags = unsafe { libc::fcntl(sys_fd, libc::F_GETFL) };
-        wasi_try_ok!((fcntl_flags >= 0).then(|| ()).ok_or(Errno::Access));
-
-        let fcntl_flags = if flags.contains(Fdflags::NONBLOCK) {
-            fcntl_flags | libc::O_NONBLOCK
-        } else {
-            fcntl_flags & (!libc::O_NONBLOCK)
+        let maybe_sys_fd = match guard.deref() {
+            Kind::File { handle, .. } => {
+                if let Some(handle) = handle {
+                    let handle = handle.clone();
+                    let handle = wasi_try_ok!(handle.read().map_err(|_| { Errno::Badf }));
+                    handle.get_special_fd()
+                } else { None }
+            },
+            _ => None,
         };
 
-        let ret = unsafe { libc::fcntl(sys_fd, libc::F_SETFL, fcntl_flags) };
-        wasi_try_ok!((ret == 0).then(|| ()).ok_or(Errno::Access));
+        if let Some(sys_fd) = maybe_sys_fd {
+            let sys_fd = wasi_try_ok!(i32::try_from(sys_fd).map_err(|_| { Errno::Badf }));
+            return set_nonblocking_mode(sys_fd, flags.contains(Fdflags::NONBLOCK));
+        }
+    }
+
+    Ok(Errno::Success)
+}
+
+#[cfg(unix)]
+fn set_nonblocking_mode(host_fd: i32, new_value: bool) -> Result<Errno, WasiError> {
+    let fcntl_flags = unsafe { libc::fcntl(host_fd, libc::F_GETFL) };
+    wasi_try_ok!((fcntl_flags >= 0).then(|| ()).ok_or(Errno::Access));
+
+    let fcntl_flags = if new_value {
+        fcntl_flags | libc::O_NONBLOCK
+    } else {
+        fcntl_flags & !libc::O_NONBLOCK
     };
+
+    let ret = unsafe { libc::fcntl(host_fd, libc::F_SETFL, fcntl_flags) };
+    wasi_try_ok!((ret == 0).then(|| ()).ok_or(Errno::Access));
 
     Ok(Errno::Success)
 }
